f1b731e1d637c09b2422038ea47ec2fa4b8618b2
niksh-19 <nikita.sharma@edu.sait.ca>
2024-11-07 09:11:10 -0700

    Event handling and state lifting

--- App.jsx
+++ App.jsx
@@ -16,10 +16,16 @@ function App() {
 
   const [tasks, setTasks] = useState(["Do laundry", "Go to gym", "Walk dog"])
 
+  const addTask = (task) => {
+    if (!tasks.includes(task)) {
+    setTasks([...tasks, task]);
+    }
+  }
+
   return (
     <SafeAreaView>
       <ToDoList tasks={tasks}/>
-      <ToDoForm/>
+      <ToDoForm addTask={addTask}/>
     </SafeAreaView>
   );
 }
--- ToDoForm.jsx
+++ ToDoForm.jsx
@@ -1,15 +1,24 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { View, TextInput, Button, StyleSheet } from 'react-native';
 
-export default function ToDoForm() {
+export default function ToDoForm({ addTask}) {
+  const [taskText, setTaskText] = useState("");
+
   return (
     <View style={styles.form}>
       <TextInput
         style={styles.input}
         placeholder="Add a new task..."
+        onChangeText={(text) => setTaskText(text)}
+        value={taskText}
       />
-      <Button title="Add" />
+      <Button title="Add"
+        onPress={() => {
+          addTask(taskText);
+          setTaskText("");
+        }}
+         />
     </View>
   );
 }
